5a7ff8f9925c7bcdfcba8d3e5b53f13df2be80d6
Sspg293 <166625624+Sspg293@users.noreply.github.com>
2026-02-10 15:20:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -145,10 +145,6 @@ def plot_chart(data, name, projection):
 
 assets = {
     "NIFTY 50": "^NSEI",
-    "Nippon Gold ETF": "GOLDBEES.NS",
-    "Nippon Silver ETF": "SILVERBEES.NS",
-    "HDFC Gold ETF": "HDFCGOLD.NS",
-    "HDFC Silver ETF": "HDFCSILVER.NS",
     "Tata Gold ETF": "TATAGOLD.NS",
     "Tata Silver ETF": "TATSILV.NS"
 }
